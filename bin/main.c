366d49e445ee708a118b9adc94cfcd425e9b67c7
xirzo <easttem@gmail.com>
2025-06-15 14:04:11 +0300

    chore: new movement scheme

--- bin/main.c
+++ bin/main.c
@@ -1,8 +1,8 @@
-#include <stddef.h>
-#include <stdio.h>
+#include "flecs.h"
 #include "input.h"
 #include "raylib.h"
-#include "flecs.h"
+#include <stddef.h>
+#include <stdio.h>
 
 #define UP_DIRECTION -1.f
 #define DOWN_DIRECTION 1.f
@@ -31,40 +31,42 @@ typedef struct RenderableRectangle {
 typedef struct PlayerInput {
   InputAction up_action;
   InputAction down_action;
-  Input* input;
+  Input *input;
 } PlayerInput;
 
-void Move(ecs_iter_t* it) {
-  Position* positions = ecs_field(it, Position, 0);
-  Velocity* velocities = ecs_field(it, Velocity, 1);
+void Move(ecs_iter_t *it) {
+  Position *positions = ecs_field(it, Position, 0);
+  Velocity *velocities = ecs_field(it, Velocity, 1);
 
-  const Properties* properties = (Properties*)it->param;
+  const Properties *properties = (Properties *)it->param;
 
   for (size_t i = 0; i < it->count; i++) {
-    positions[i].x += velocities[i].x * it->delta_time * properties->PADDLE_SPEED;
-    positions[i].y += velocities[i].y * it->delta_time * properties->PADDLE_SPEED;
+    positions[i].x +=
+        velocities[i].x * it->delta_time * properties->PADDLE_SPEED;
+    positions[i].y +=
+        velocities[i].y * it->delta_time * properties->PADDLE_SPEED;
   }
 }
 
-void RenderRectangle(ecs_iter_t* it) {
-  Position* positions = ecs_field(it, Position, 0);
-  RenderableRectangle* rectangles = ecs_field(it, RenderableRectangle, 1);
+void RenderRectangle(ecs_iter_t *it) {
+  Position *positions = ecs_field(it, Position, 0);
+  RenderableRectangle *rectangles = ecs_field(it, RenderableRectangle, 1);
 
   for (size_t i = 0; i < it->count; i++) {
-    Position* p = &positions[i];
-    RenderableRectangle* r = &rectangles[i];
+    Position *p = &positions[i];
+    RenderableRectangle *r = &rectangles[i];
 
     DrawRectangle(p->x, p->y, r->width, r->height, r->color);
   }
 }
 
-void HandlePlayerInputActions(ecs_iter_t* it) {
-  Velocity* velocities = ecs_field(it, Velocity, 0);
-  PlayerInput* inputs = ecs_field(it, PlayerInput, 1);
+void HandlePlayerInputActions(ecs_iter_t *it) {
+  Velocity *velocities = ecs_field(it, Velocity, 0);
+  PlayerInput *inputs = ecs_field(it, PlayerInput, 1);
 
   for (size_t i = 0; i < it->count; i++) {
     velocities[i].y = 0;
-    Input* input = inputs[i].input;
+    Input *input = inputs[i].input;
 
     if (IsActionPressed(input, inputs[i].up_action)) {
       velocities[i].y = UP_DIRECTION;
@@ -89,7 +91,7 @@ int main(void) {
   InitWindow(properties.SCREEN_WIDTH, properties.SCREEN_HEIGHT, "game");
   SetTargetFPS(properties.FPS_LOCK);
 
-  ecs_world_t* world = ecs_init();
+  ecs_world_t *world = ecs_init();
 
   ECS_COMPONENT(world, Position);
   ECS_COMPONENT(world, Velocity);
@@ -97,43 +99,48 @@ int main(void) {
   ECS_COMPONENT(world, PlayerInput);
 
   ECS_SYSTEM(world, Move, EcsOnUpdate, Position, [in] Velocity);
-  ECS_SYSTEM(world, RenderRectangle, EcsOnUpdate, [in] Position, [in] RenderableRectangle);
-  ECS_SYSTEM(world, HandlePlayerInputActions, EcsOnUpdate, [out] Velocity, [in] PlayerInput);
+  ECS_SYSTEM(world, RenderRectangle,
+             EcsOnUpdate, [in] Position, [in] RenderableRectangle);
+  ECS_SYSTEM(world, HandlePlayerInputActions,
+             EcsOnUpdate, [out] Velocity, [in] PlayerInput);
 
   Input left_paddle_input;
   InitInput(&left_paddle_input);
-  left_paddle_input.bindings[ACTION_MOVE_UP].key = KEY_W;
-  left_paddle_input.bindings[ACTION_MOVE_DOWN].key = KEY_S;
+  left_paddle_input.bindings[ACTION_MOVE_UP].key = KEY_S;
+  left_paddle_input.bindings[ACTION_MOVE_DOWN].key = KEY_D;
 
   Input right_paddle_input;
   InitInput(&right_paddle_input);
   right_paddle_input.bindings[ACTION_MOVE_UP].key = KEY_J;
   right_paddle_input.bindings[ACTION_MOVE_DOWN].key = KEY_K;
 
-  ecs_entity_t left_paddle = ecs_insert(
-      world,
-      ecs_value(Position, {properties.PADDLE_SCREEN_SIZE_MARGIN,
-                           (float)properties.SCREEN_HEIGHT / 2 - properties.PADDLE_HEIGHT / 2}),
-      ecs_value(Velocity, {0, 0}),
-      ecs_value(RenderableRectangle,
-                {
-                    .width = properties.PADDLE_WIDTH,
-                    .height = properties.PADDLE_HEIGHT,
-                    .color = RED,
-                }),
-      ecs_value(PlayerInput,
-                {
-                    .up_action = ACTION_MOVE_UP,
-                    .down_action = ACTION_MOVE_DOWN,
-                    .input = &left_paddle_input,
-                })
+  ecs_entity_t left_paddle =
+      ecs_insert(world,
+                 ecs_value(Position, {properties.PADDLE_SCREEN_SIZE_MARGIN,
+                                      (float)properties.SCREEN_HEIGHT / 2 -
+                                          properties.PADDLE_HEIGHT / 2}),
+                 ecs_value(Velocity, {0, 0}),
+                 ecs_value(RenderableRectangle,
+                           {
+                               .width = properties.PADDLE_WIDTH,
+                               .height = properties.PADDLE_HEIGHT,
+                               .color = RED,
+                           }),
+                 ecs_value(PlayerInput,
+                           {
+                               .up_action = ACTION_MOVE_UP,
+                               .down_action = ACTION_MOVE_DOWN,
+                               .input = &left_paddle_input,
+                           })
 
-  );
+      );
 
   ecs_entity_t right_paddle = ecs_insert(
       world,
-      ecs_value(Position, {properties.SCREEN_WIDTH - properties.PADDLE_SCREEN_SIZE_MARGIN,
-                           (float)properties.SCREEN_HEIGHT / 2 - properties.PADDLE_HEIGHT / 2}),
+      ecs_value(
+          Position,
+          {properties.SCREEN_WIDTH - properties.PADDLE_SCREEN_SIZE_MARGIN,
+           (float)properties.SCREEN_HEIGHT / 2 - properties.PADDLE_HEIGHT / 2}),
       ecs_value(Velocity, {0, 0}),
       ecs_value(RenderableRectangle,
                 {
@@ -155,7 +162,7 @@ int main(void) {
     UpdateInput(&right_paddle_input);
 
     ecs_run(world, ecs_id(HandlePlayerInputActions), GetFrameTime(), NULL);
-    ecs_run(world, ecs_id(Move), GetFrameTime(), (void*)&properties);
+    ecs_run(world, ecs_id(Move), GetFrameTime(), (void *)&properties);
 
     BeginDrawing();
 
