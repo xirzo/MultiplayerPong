8e2965bec8b03fe303b121b893e7584faa705f99
xirzo <easttem@gmail.com>
2026-07-10 22:42:14 +0300

    chore: remove lib/ bin/

--- lib/client_state.c
+++
@@ -1,5 +0,0 @@
-#include "client_state.h"
-
-void InitClientState(ClientState* s) {
-  s->current_state = kMainMenu;
-}
--- lib/player.h
+++
@@ -1,8 +0,0 @@
-#ifndef PLAYER_H
-#define PLAYER_H
-
-typedef struct {
-  int is_main;
-} Player;
-
-#endif  // !PLAYER_H
